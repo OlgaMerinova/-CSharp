506d163ba947866d23f2a6edfc20164786737379
olja4567 <olja4567@yandex.ru>
2022-08-23 11:48:49 +0300

    task 8

---
+++ task8/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int x = 1;
+Console.Write("Write number N: ");
+int y = int.Parse(Console.ReadLine());
+for (int z = x; z <= y; z++)
+{
+    if (z % 2 == 0)
+    {
+        Console.Write(z + " ");
+    }
+}
+Console.ReadKey();
+
+
+
+
+
