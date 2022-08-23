d70d0d0607dc9c1050a67aa50792b49b7b1a5555
olja4567 <olja4567@yandex.ru>
2022-08-23 11:49:45 +0300

    task 6

---
+++ task6/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 4;
+
+if(a%2 == 0)
+{ 
+    Console.WriteLine("Yes");
+}
+else
+Console.WriteLine("No");
+
+int b = -3;
+
+if(b%2 == 0)
+{ 
+    Console.WriteLine("Yes");
+}
+else
+Console.WriteLine("No");
+
+int c = 7;
+
+if(c%2 == 0)
+{ 
+    Console.WriteLine("Yes");
+}
+else
+Console.WriteLine("No");
