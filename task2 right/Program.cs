50a48421f6956d641d4222097e33b8f489dccb89
olja4567 <olja4567@yandex.ru>
2022-08-23 12:03:29 +0300

    task2 right

---
+++ task2 right/Program.cs
@@ -0,0 +1,42 @@
+﻿int num1 = 5;
+int num2 = 7;
+
+if (num1 > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
+
+int num3 = 2;
+int num4 = 10;
+
+if (num3 > num4)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num3);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num4);
+}
+
+
+int num5 = -9;
+int num6 = -3;
+
+if (num5 > num6)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num5);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num6);
+}
